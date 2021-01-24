3805a3b8ea8ffd1d5afa98016c590a257aa15c73
Anilton Araujo <aniltonaraujo01@gmail.com>
2021-01-24 09:43:03 -0300

    tela de escolha de pagamento

---
+++ src/pages/payment/payment.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { PedidoDTO } from '../../models/pedido.dto';
+
+@IonicPage()
+@Component({
+  selector: 'page-payment',
+  templateUrl: 'payment.html',
+})
+export class PaymentPage {
+
+  pedido: PedidoDTO;
+  parcelas: number[] = [1,2,3,4,5,6];
+  formGroup: FormGroup;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public formBuild: FormBuilder) {
+
+      this.pedido = this.navParams.get('pedido');
+
+      this.formGroup = this.formBuild.group({
+        numeroDeParcelas: [1, Validators.required],
+        "@type": ["pagamentoComCartao", Validators.required]
+      });
+  }
+
+nextPage(){
+  this.pedido.pagamento = this.formGroup.value;
+  console.log(this.pedido);
+}
+
+}
